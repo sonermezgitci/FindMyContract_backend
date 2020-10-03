7fd8de5cf0edee2d9fe2088d9d9b356232050f1e
Soner Mezgitci <mezgitci9@gmail.com>
2020-10-02 21:41:59 -0400

    Deleted set up

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,9 +1,9 @@
 class ContractsController < ApplicationController
  
-   before_action :set_player
+   # before_action :set_player
 
     def index 
-      @contracts = @player.contracts 
+      @contracts = Contract.all
       render json: @contracts 
     end 
 
@@ -14,25 +14,33 @@ class ContractsController < ApplicationController
      end 
     
      def create 
-        @contract = @player.contracts.new(contract_params)
-        @contract.save
-        render json: @player
+     @contract = Contract.create(contract_params)
+     render json: @contract
+     end 
 
-    end 
+
+      #   @contract = @player.contracts.new(contract_params)
+      #   @contract.save
+      #   render json: @player
+
+   #  end 
     
    
 
     def destroy 
+       @contract = Contract.find(params[:id])
        
+       @contract.destroy
+       render json: @contract
     end 
 
     private 
-     def set_player
-        @player = Player.find(params[:player_id])
+   #   def set_player
+   #      @player = Player.find(params[:player_id])
 
-     end
+   #   end
 
     def contract_params 
-    params.require(:contract).permit(:name, :team_name, :start_date, :expiration_date, :amount, :player_id, :team_id)
+    params.require(:contract).permit(:player_name, :team_name, :start_date, :expiration_date, :amount, :bonus, :salary, :player_id, :team_id)
     end 
 end
